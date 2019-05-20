35bae6f67a525c003fa506dbeaad952a8be379d6
Paxon96 <bwloczyk@gmail.com>
2019-05-20 14:33:43 +0200

    refactoring window code

--- src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/CommandManagerWindow.java
@@ -1,18 +1,7 @@
 package edu.kis.powp.jobs2d.command.gui;
 
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.*;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.kis.powp.appbase.Application;
 import edu.kis.powp.appbase.gui.WindowComponent;
 import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
@@ -20,134 +9,140 @@ import edu.kis.powp.jobs2d.command.manager.SingleCommand;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.observer.Subscriber;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandManagerWindow extends JFrame implements WindowComponent {
 
-	private DriverCommandManager commandManager;
+    private DriverCommandManager commandManager;
 
-	private JTextArea currentCommandField;
-	private Application application;
-	private JTextArea newCommand;
+    private JTextArea currentCommandField;
+    private JTextArea newCommand;
 
-	private String observerListString;
-	private JTextArea observerListField;
+    private String observerListString;
+    private JTextArea observerListField;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 9204679248304669948L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 9204679248304669948L;
 
-	public CommandManagerWindow(DriverCommandManager commandManager, Application application) {
-		this.application = application;
-		this.setTitle("Command Manager");
-		this.setSize(400, 400);
-		Container content = this.getContentPane();
-		content.setLayout(new GridBagLayout());
+    public CommandManagerWindow(DriverCommandManager commandManager) {
+        this.setTitle("Command Manager");
+        this.setSize(400, 400);
+        Container content = this.getContentPane();
+        content.setLayout(new GridBagLayout());
 
-		this.commandManager = commandManager;
+        this.commandManager = commandManager;
 
-		GridBagConstraints c = new GridBagConstraints();
+        GridBagConstraints c = new GridBagConstraints();
 
-		observerListField = new JTextArea("");
-		observerListField.setEditable(false);
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(observerListField, c);
-		updateObserverListField();
+        observerListField = new JTextArea("");
+        observerListField.setEditable(false);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(observerListField, c);
+        updateObserverListField();
 
-		currentCommandField = new JTextArea("");
-		currentCommandField.setEditable(false);
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(currentCommandField, c);
-		updateCurrentCommandField();
+        currentCommandField = new JTextArea("");
+        currentCommandField.setEditable(false);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(currentCommandField, c);
+        updateCurrentCommandField();
 
+        newCommand = new JTextArea(5, 20);
+        JScrollPane scrollPane = new JScrollPane(newCommand);
+        newCommand.setEditable(true);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(scrollPane, c);
 
-		newCommand = new JTextArea(5,20);
-		JScrollPane scrollPane = new JScrollPane(newCommand);
-		newCommand.setEditable(true);
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(scrollPane, c);
+        JButton btnAddCommand = new JButton("Add command");
+        btnAddCommand.addActionListener((ActionEvent e) -> this.addCommand());
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(btnAddCommand, c);
 
-		JButton btnAddCommand = new JButton("Add command");
-		btnAddCommand.addActionListener((ActionEvent e) -> this.addCommand());
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(btnAddCommand, c);
+        JButton btnClearCommand = new JButton("Clear command");
+        btnClearCommand.addActionListener((ActionEvent e) -> this.clearCommand());
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(btnClearCommand, c);
 
-		JButton btnClearCommand = new JButton("Clear command");
-		btnClearCommand.addActionListener((ActionEvent e) -> this.clearCommand());
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(btnClearCommand, c);
+        JButton btnClearObservers = new JButton("Delete observers");
+        btnClearObservers.addActionListener((ActionEvent e) -> this.deleteObservers());
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.gridx = 0;
+        c.weighty = 1;
+        content.add(btnClearObservers, c);
+    }
 
-		JButton btnClearObservers = new JButton("Delete observers");
-		btnClearObservers.addActionListener((ActionEvent e) -> this.deleteObservers());
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 1;
-		c.gridx = 0;
-		c.weighty = 1;
-		content.add(btnClearObservers, c);
-	}
+    private void clearCommand() {
+        commandManager.clearCurrentCommand();
+        updateCurrentCommandField();
+    }
 
-	private void clearCommand() {
-		commandManager.clearCurrentCommand();
-		updateCurrentCommandField();
-	}
+    public void updateCurrentCommandField() {
+        currentCommandField.setText(commandManager.getCurrentCommandString());
+    }
 
-	public void updateCurrentCommandField() {
-		currentCommandField.setText(commandManager.getCurrentCommandString());
-	}
+    public void deleteObservers() {
+        commandManager.getChangePublisher().clearObservers();
+        this.updateObserverListField();
+    }
 
-	public void deleteObservers() {
-		commandManager.getChangePublisher().clearObservers();
-		this.updateObserverListField();
-	}
+    private void updateObserverListField() {
+        observerListString = "";
+        List<Subscriber> commandChangeSubscribers = commandManager.getChangePublisher().getSubscribers();
+        for (Subscriber observer : commandChangeSubscribers) {
+            observerListString += observer.toString() + System.lineSeparator();
+        }
+        if (commandChangeSubscribers.isEmpty())
+            observerListString = "No observers loaded";
 
-	private void updateObserverListField() {
-		observerListString = "";
-		List<Subscriber> commandChangeSubscribers = commandManager.getChangePublisher().getSubscribers();
-		for (Subscriber observer : commandChangeSubscribers) {
-			observerListString += observer.toString() + System.lineSeparator();
-		}
-		if (commandChangeSubscribers.isEmpty())
-			observerListString = "No observers loaded";
+        observerListField.setText(observerListString);
+    }
 
-		observerListField.setText(observerListString);
-	}
+    private void addCommand() {
+        String newCommandText = newCommand.getText();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<SingleCommand> singleCommands = objectMapper.readValue(newCommandText, new TypeReference<List<SingleCommand>>() {
 
-	private void addCommand() {
-		String newCommandText = newCommand.getText();
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-			List<SingleCommand> singleCommands = objectMapper.readValue(newCommandText, new TypeReference<List<SingleCommand>>(){});
-			List<DriverCommand> driverCommands = new ArrayList<>();
-			singleCommands.forEach(e -> driverCommands.add(e.getCommand()));
-			DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
-			manager.setCurrentCommand(driverCommands, "TopSecretCommand");
-		} catch (IOException e) {
-			currentCommandField.setText("Wrong JSON format");
-		}
-	}
+            });
+            List<DriverCommand> driverCommands = new ArrayList<>();
+            singleCommands.forEach(e -> driverCommands.add(e.getCommand()));
+            DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
+            manager.setCurrentCommand(driverCommands, "TopSecretCommand");
+        } catch (IOException e) {
+            currentCommandField.setText("Wrong JSON format");
+        }
+    }
 
-	@Override
-	public void HideIfVisibleAndShowIfHidden() {
-		updateObserverListField();
-		if (this.isVisible()) {
-			this.setVisible(false);
-		} else {
-			this.setVisible(true);
-		}
-	}
+    @Override
+    public void HideIfVisibleAndShowIfHidden() {
+        updateObserverListField();
+        if (this.isVisible()) {
+            this.setVisible(false);
+        } else {
+            this.setVisible(true);
+        }
+    }
 
 }
