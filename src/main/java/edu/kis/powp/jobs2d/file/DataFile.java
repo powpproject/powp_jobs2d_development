cc6783c357156160a969c8dca7021bfe0b0d2c03
Piotrek <piotr.marczewski95@gmail.com>
2019-05-20 14:32:34 +0200

    Create save and read file

--- src/main/java/edu/kis/powp/jobs2d/file/DataFile.java
+++ src/main/java/edu/kis/powp/jobs2d/file/DataFile.java
@@ -2,6 +2,8 @@ package edu.kis.powp.jobs2d.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DataFile {
@@ -33,6 +35,17 @@ public class DataFile {
 	}
 	
 	public void saveData() {
-		
+		File fnew=new File("data.txt");
+		FileWriter f2;
+
+		try 
+		{
+		    f2 = new FileWriter(fnew,false);
+		    f2.write("");  // <==== wrzucic tu stan zuzycia
+	
+		    f2.close();
+		} catch (IOException e) {
+		        e.printStackTrace();
+		} 
 	}
 }
